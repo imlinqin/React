b640669e5aadb09562ccd87b43383d9777902b63
linqin <imlinqin@qq.com>
2016-08-14 11:48:35 +0800

    wefef

--- src/React-Hot/Yqf/js/json.js
+++ src/React-Hot/Yqf/js/json.js
@@ -3,8 +3,16 @@
   {
     "url": "33",
     "forks_url": "5g5",
+    "flightname": 'hahah',
+    "truncated": false
+  }
+  ],
+  [
+  {
+    "url": "33",
+    "forks_url": "5g5",
 
     "truncated": false
   }
-]
+  ]
 ]

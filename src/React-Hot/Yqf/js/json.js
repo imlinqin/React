53c48b38c16e60e59e2a558723bf54ddbb42c295
linqin <imlinqin@qq.com>
2016-08-14 11:39:30 +0800

    ddd

--- src/React-Hot/Yqf/js/json.js
+++ src/React-Hot/Yqf/js/json.js
@@ -3,8 +3,18 @@
   {
     "url": "33rr",
     "forks_url": "5hh5",
-  
-    "truncated": false
+
+    "truncated": false,
+    "depDate": "12:35",
+    "arriveDAte": "18:11",
+    "depTerminal": "悲观新白云机场",
+    "arriveTerminal": "阳江坡樟宜机场",
+    "airline": { "logo": "http://airlineico.b0.upaiyun.com/TZ.png!16x16", "name": "国d泰航空" },
+    "flightNo": "A380",
+    "flightTime": "8h3m",
+    "price": "3689",
+    "tax": "269",
+    "monetaryUnit": "&yen;",
   }
- ]
+  ]
 ]

471f252c4637c969f883628573b3c7a227486ed3
linqin <imlinqin@qq.com>
2016-08-14 11:42:02 +0800

    dd

--- src/React-Hot/Yqf/js/json.js
+++ src/React-Hot/Yqf/js/json.js
@@ -5,15 +5,9 @@
     "forks_url": "5hh5",
 
     "truncated": false,
-    "depDate": "12:35",
-    "arriveDAte": "18:11",
-    "depTerminal": "悲观新白云机场",
-    "arriveTerminal": "阳江坡樟宜机场",
-    "flightNo": "A380",
-    "flightTime": "8h3m",
+
     "price": "3689",
     "tax": "269",
-    "monetaryUnit": "&yen;",
   }
   ]
 ]
